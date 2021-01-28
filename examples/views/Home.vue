2b6e08e816c477457ae8e8ea202d5f3da55d34a1
Calvester <736147366@qq.com>
2021-01-29 00:42:51 +0800

    :new: 完成基础按钮组件

--- examples/views/Home.vue
+++ examples/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="home">
-      <fl-button type="primary">button</fl-button>
+    <fl-button>默认按钮</fl-button>
+    <fl-button type="primary">主要按钮</fl-button>
+    <fl-button type="warning">警告按钮</fl-button>
+    <fl-button type="danger">危险按钮</fl-button>
+    <fl-button type="success">成功按钮</fl-button>
+    <fl-button type="inverse">逆向按钮</fl-button>
+    <fl-button type="info">信息按钮</fl-button>
+    <fl-button disabled>禁用按钮</fl-button>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Home'
-}
+  name: "Home",
+};
 </script>
+
+<style lang="scss" scoped>
+.fl-button {
+  margin-right: 10px;
+}
+</style>
