8fb9af571b146b2927da8271f1d78407404ca9c1
Ricky <frankiewb9527@gmail.com>
2022-09-03 14:18:41 +0800

    :wrench: 更新 .eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,24 +2,21 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
+  root: true,
+  env: { node: true },
+  extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+    "@vue/eslint-config-prettier",
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest"
+  parserOptions: {
+    ecmaVersion: "latest",
   },
-  "overrides": [
+  overrides: [
     {
-      "files": [
-        "cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"
-      ],
-      "extends": [
-        "plugin:cypress/recommended"
-      ]
-    }
-  ]
-}
+      files: ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
+      extends: ["plugin:cypress/recommended"],
+    },
+  ],
+};
