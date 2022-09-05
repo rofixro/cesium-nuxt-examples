7a5274144a52f86945b24366a94516ba7dfd58b4
Ricky <frankiewb9527@gmail.com>
2022-09-05 10:09:53 +0800

    :wrench: 更新 eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,5 +18,11 @@ module.exports = {
       files: ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
       extends: ["plugin:cypress/recommended"],
     },
+    {
+      files: ["*.vue"],
+      rules: {
+        "vue/multi-word-component-names": 0,
+      },
+    },
   ],
 };
